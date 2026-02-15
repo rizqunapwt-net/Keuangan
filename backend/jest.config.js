e386cc2341578c4ec334cb5bc20aaf3e376f48e2
Tholib <tholibadilmaruf.campus@gmail.com>
2026-02-15 23:31:50 +0700

    stabilize local test workflow for backend and frontend

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/*.test.js'],
     verbose: true,
-    forceExit: true,
     clearMocks: true,
     resetMocks: true,
     restoreMocks: true
